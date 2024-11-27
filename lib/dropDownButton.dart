f9abd19d68aa0b391474f9c5b18644069678aaff
takefekat <a.t.h.t.fumi@gmail.com>
2024-11-27 10:41:25 +0900

    ドロップダウンボタンの位置、文字サイズ修正

--- lib/dropDownButton.dart
+++ lib/dropDownButton.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'common.dart';
 
-const double FONT_SIZE = 30;
+const double FONT_SIZE = 40;
 
 class CustomDropdown extends StatelessWidget {
   final int selectedValue;
@@ -29,35 +29,42 @@ class CustomDropdown extends StatelessWidget {
           ),
         ],
       ),
-      child: DropdownButton(
-        items: const [
-          DropdownMenuItem<int>(
-            value: PATH_MODE_MANUAL,
-            child: Center(
-                child: Text('手動', style: TextStyle(fontSize: FONT_SIZE))),
-          ),
-          DropdownMenuItem<int>(
-            value: PATH_MODE_AUTO,
-            child: Center(
-                child: Text('まっすぐ', style: TextStyle(fontSize: FONT_SIZE))),
-          ),
-          DropdownMenuItem<int>(
-            value: PATH_MODE_RANDOM,
-            child: Center(
-                child: Text('ランダム', style: TextStyle(fontSize: FONT_SIZE))),
-          ),
-          DropdownMenuItem<int>(
-            value: PATH_MODE_OFF,
-            child: Center(
-                child: Text('OFF', style: TextStyle(fontSize: FONT_SIZE))),
-          ),
-        ],
-        onChanged: onChanged,
-        value: selectedValue,
-        elevation: 16,
-        menuWidth: 160,
-        isExpanded: true, // ドロップダウンの幅を広げる
-        underline: const SizedBox.shrink(), // 枠線を消す
+      child: Align(
+        alignment: Alignment.center,
+        child: DropdownButton(
+          items: const [
+            DropdownMenuItem<int>(
+              value: PATH_MODE_MANUAL,
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text('手動', style: TextStyle(fontSize: FONT_SIZE))),
+            ),
+            DropdownMenuItem<int>(
+              value: PATH_MODE_AUTO,
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text('まっすぐ', style: TextStyle(fontSize: FONT_SIZE))),
+            ),
+            DropdownMenuItem<int>(
+              value: PATH_MODE_RANDOM,
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text('ランダム', style: TextStyle(fontSize: FONT_SIZE))),
+            ),
+            DropdownMenuItem<int>(
+              value: PATH_MODE_OFF,
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text('OFF', style: TextStyle(fontSize: FONT_SIZE))),
+            ),
+          ],
+          onChanged: onChanged,
+          value: selectedValue,
+          elevation: 16,
+          menuWidth: 160,
+          isExpanded: true, // ドロップダウンの幅を広げる
+          underline: const SizedBox.shrink(), // 枠線を消す
+        ),
       ),
     );
   }
